c04d432e3b2b942bd2533801095c363f6e3d8ea6
Patryk Szafranek <patryk.szafranek94@gmail.com>
2023-05-08 22:44:31 +0200

    first commit for review

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
-const app = require("./app");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const app = require("./app.js");
+const uriDb =
+  "mongodb+srv://test:test@cluster0.q0cc5xk.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
+const connection = mongoose.connect(uriDb, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+
+connection
+  .then(() => {
+    app.listen(PORT, function () {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((error) => {
+    console.log("Database connection error:", error);
+    process.exit(1);
+  });
