1fc51bf92b30d0ad0eea8b93203c5c21fb10ce6f
Patryk Szafranek <patryk.szafranek94@gmail.com>
2023-05-21 18:45:05 +0200

    first commit

--- repository/Contacts.js
+++ repository/Contacts.js
@@ -1,4 +1,4 @@
-const Contact = require('../model/contact');
+const Contact = require("../model/contact");
 
 const listContacts = async (userId, query) => {
   const { sortBy, sortByDesc, filter, limit = 5, offset = 0 } = query;
@@ -11,10 +11,10 @@ const listContacts = async (userId, query) => {
       ...(sortBy ? { [`${sortBy}`]: 1 } : {}), // { name: 1 }
       ...(sortByDesc ? { [`${sortByDesc}`]: -1 } : {}),
     },
-    select: filter ? filter.split('|').join(' ') : '',
+    select: filter ? filter.split("|").join(" ") : "",
     populate: {
-      path: 'owner',
-      select: 'name email gender createdAt updatedAt',
+      path: "owner",
+      select: "name email gender createdAt updatedAt",
     },
   });
   const { docs: contacts } = results;
@@ -27,27 +27,27 @@ const getContactById = async (contactId, userId) => {
     _id: contactId,
     owner: userId,
   }).populate({
-    path: 'owner',
-    select: 'name email gender createdAt updatedAt',
+    path: "owner",
+    select: "name email gender createdAt updatedAt",
   });
   return result;
 };
 
-const removeContact = async contactId => {
+const removeContact = async (contactId) => {
   const result = await Contact.findByIdAndRemove({ _id: contactId });
   return result;
 };
 
-const addContact = async body => {
+const addContact = async (body) => {
   const result = await Contact.create(body);
   return result;
 };
 
 const updateContact = async (contactId, body) => {
   const result = await Contact.findByIdAndUpdate(
-      { _id: contactId },
-      { ...body },
-      { new: true },
+    { _id: contactId },
+    { ...body },
+    { new: true }
   );
   return result;
 };
--- repository/users.js
+++ repository/users.js
@@ -1,19 +1,23 @@
-const User = require('../model/user');
+const User = require("../model/user");
 
-const findById = async id => {
-    return await User.findById(id);
+const findById = async (id) => {
+  return await User.findById(id);
 };
 
-const findByEmail = async email => {
-    return await User.findOne({ email });
+const findByEmail = async (email) => {
+  return await User.findOne({ email });
 };
 
-const create = async options => {
-    const user = new User(options);
-    return await user.save();
+const create = async (options) => {
+  const user = new User(options);
+  return await user.save();
 };
 
 const updateToken = async (id, token) => {
-    return await User.updateOne({ _id: id }, { token });
+  return await User.updateOne({ _id: id }, { token });
 };
-module.exports = { findById, findByEmail, create, updateToken };
+
+const updateAvatar = async (id, avatar, idUserCloud = null) => {
+  return await User.updateOne({ _id: id }, { avatar, idUserCloud });
+};
+module.exports = { findById, findByEmail, create, updateToken, updateAvatar };
